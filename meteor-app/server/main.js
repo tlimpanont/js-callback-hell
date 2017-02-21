b81072338d921b8412db8511b1091ed701400f68
Theuy Limpanont <info@theuy.nl>
2017-02-21 12:03:44 +0100

    docs(add callback note):

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -1,37 +1,37 @@
 import {Meteor} from 'meteor/meteor';
 
-/* #1 */
-Meteor.startup(function() {
-  var mongoose = require('mongoose');
-  mongoose.connect('mongodb://localhost:3001/local');
-  var userSchema = mongoose.Schema({
-    name: String
-  });
-  var User = mongoose.model('users', userSchema);
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-  /* #2 */
-  db.once('open', function() {
-    /* #3 */
-    User.remove({}, function(err, users) {
-      if(err) {
-        throw new Error(err);
-      }
-      let theuy = new User({name: "Theuy Limpanont"});
-      let hans = new User({name: "Hans Kramer"});
-      /* #4 */
-      User.collection.insert([theuy, hans], function(err, docs) {
-        if(err) {
-          throw new Error(err);
-        }
-        /* #5 */
-        User.find({}, function(err, users) {
-          if(err) {
-            throw new Error(err);
-          }
-          console.log(users);
-        });
-      });
+/* CALLBACK #1 */
+Meteor.startup(function () {
+    let mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost:3001/local');
+    let userSchema = mongoose.Schema({
+        name: String
     });
-  });
+    let User = mongoose.model('users', userSchema);
+    let db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    /* CALLBACK #2 */
+    db.once('open', function () {
+        /* CALLBACK #3 */
+        User.remove({}, function (err, users) {
+            if (err) {
+                throw new Error(err);
+            }
+            let theuy = new User({name: "Theuy Limpanont"});
+            let hans = new User({name: "Hans Kramer"});
+            /* CALLBACK #4 */
+            User.collection.insert([theuy, hans], function (err, docs) {
+                if (err) {
+                    throw new Error(err);
+                }
+                /* CALLBACK #5 */
+                User.find({}, function (err, users) {
+                    if (err) {
+                        throw new Error(err);
+                    }
+                    console.log(users);
+                });
+            });
+        });
+    });
 });
