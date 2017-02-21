88680541a42c10c81d166038ac56d7a51a7a545a
Theuy Limpanont <info@theuy.nl>
2017-02-21 14:26:16 +0100

    feat(server/main): replace promise interface with observables

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -1,78 +1,73 @@
 import {Meteor} from 'meteor/meteor';
+import Rx from 'rxjs/Rx';
 let mongoose = require('mongoose');
 let userSchema = mongoose.Schema({name: String});
 let User = mongoose.model('users', userSchema);
 
-function connectToMongooseDB() {
-    return new Promise(function (resolve, reject) {
-        mongoose.connect('mongodb://localhost:3001/local');
-        let db = mongoose.connection;
-        db.on('error', function (error) {
-            reject(error);
-        });
-        db.once('open', function () {
-            resolve(db);
-        });
+let connectToMongooseDB$ = new Rx.Observable(function (observer) {
+    mongoose.connect('mongodb://localhost:3001/local');
+    let db = mongoose.connection;
+    db.on('error', function (err) {
+        observer.error(err);
     });
-}
+    db.once('open', function () {
+        observer.next(db);
+    });
+});
 
-function removeAllUsers() {
-    return new Promise(function (resolve, reject) {
-        User.remove({}, function (err, users) {
-            if (err) {
-                reject(err);
-            }
-            resolve(users)
-        });
+let removeAllUsers$ = new Rx.Observable(function (observer) {
+    User.remove({}, function (err, users) {
+        if (err) {
+            observer.error(err);
+        }
+        observer.next(users);
     });
-}
+});
 
-function insertTestUsers() {
-    return new Promise(function (resolve, reject) {
-        let theuy = new User({name: "Theuy Limpanont"});
-        let hans = new User({name: "Hans Kramer"});
-        User.collection.insert([theuy, hans], function (err, result) {
-            if (err) {
-                reject(err);
-            }
-            resolve(result)
-        });
-    });
-}
 
-function findAllUsers() {
-    return new Promise(function (resolve, reject) {
-        User.find({}, function (err, users) {
-            if (err) {
-                reject(err);
-            }
-            resolve(users)
-        });
+let insertTestUsers$ = new Rx.Observable(function (observer) {
+    let theuy = new User({name: "Theuy Limpanont"});
+    let hans = new User({name: "Hans Kramer"});
+    User.collection.insert([theuy, hans], function (err, result) {
+        if (err) {
+            observer.error(err);
+        }
+        observer.next(result);
     });
-}
+});
 
-function meteorStartUp() {
-    return new Promise(function (resolve, reject) {
-        Meteor.startup(function () {
-            resolve();
-        });
+
+let findAllUsers$ = new Rx.Observable(function (observer) {
+    User.find({}, function (err, users) {
+        if (err) {
+            observer.error(err);
+        }
+        observer.next(users);
     });
-}
+});
+
+let meteorStartUp$ = new Rx.Observable(function (observer) {
+    Meteor.startup(function () {
+        observer.next();
+    });
+});
+
 
 /** ACTION FLOWS FROM TOP TO BOTTOM **/
-Promise.all([
-    meteorStartUp(),
-    connectToMongooseDB(),
-    removeAllUsers(),
-    insertTestUsers(),
-    findAllUsers()
-])
-    .then(function ([meteorStartup, db, removeUsers, insertedUsers, allUsers]) {
+Rx.Observable.combineLatest(
+    meteorStartUp$,
+    connectToMongooseDB$,
+    removeAllUsers$,
+    insertTestUsers$,
+    findAllUsers$
+)
+    .subscribe(function ([meteorStartup, db, removeUsers, insertedUsers, allUsers]) {
         console.log("dbHost: ", db.host);
         console.log("dbPort: ", db.port);
         console.log("allUsers: ", allUsers);
-    })
-    .catch(function (error) {
-        console.error("error from promise catch: ", error);
+    }, function (error) {
+        console.error("error from observable catch: ", error);
+    }, function () {
+        console.log("stream completed!");
     });
 /** END FLOWS FROM TOP TO BOTTOM **/
