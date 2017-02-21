8c7e9d45459f70e1ab6c0d94b176f19fa1763cdf
Theuy Limpanont <info@theuy.nl>
2017-02-21 13:28:54 +0100

    refactor(promisfy the callbacks):

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -2,51 +2,76 @@ import {Meteor} from 'meteor/meteor';
 let mongoose = require('mongoose');
 let userSchema, User, db;
 
-/** ACTION FLOWS FROM TOP TO BOTTOM **/
-function meteorStartedUp() {
-
-    mongoose.connect('mongodb://localhost:3001/local');
-    userSchema = mongoose.Schema({
-        name: String
+function connectToMongooseDB() {
+    return new Promise(function (resolve, reject) {
+        mongoose.connect('mongodb://localhost:3001/local');
+        userSchema = mongoose.Schema({
+            name: String
+        });
+        User = mongoose.model('users', userSchema);
+        db = mongoose.connection;
+        db.on('error', function (error) {
+            reject(error);
+        });
+        db.once('open', function () {
+            resolve(db);
+        });
     });
-    User = mongoose.model('users', userSchema);
-    db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-
-    /* CALLBACK #2 */
-    db.once('open', mongooseDBOpened);
 }
 
-function mongooseDBOpened() {
-    /* CALLBACK #3 */
-    User.remove({}, allUsersRemoved);
+function removeAllUsers() {
+    return new Promise(function (resolve, reject) {
+        User.remove({}, function (err, users) {
+            if (err) {
+                reject(err);
+            }
+            resolve(users)
+        });
+    });
 }
 
-function allUsersRemoved(err, users) {
-    if (err) {
-        throw new Error(err);
-    }
-    let theuy = new User({name: "Theuy Limpanont"});
-    let hans = new User({name: "Hans Kramer"});
-    /* CALLBACK #4 */
-    User.collection.insert([theuy, hans], testUsersInserted);
+function insertTestUsers() {
+    return new Promise(function (resolve, reject) {
+        let theuy = new User({name: "Theuy Limpanont"});
+        let hans = new User({name: "Hans Kramer"});
+        User.collection.insert([theuy, hans], function (err, result) {
+            if (err) {
+                reject(err);
+            }
+            resolve(result)
+        });
+    });
 }
 
-function testUsersInserted(err, docs) {
-    if (err) {
-        throw new Error(err);
-    }
-    /* CALLBACK #5 */
-    User.find({}, allUsersFound);
+function findAllUsers() {
+    return new Promise(function (resolve, reject) {
+        User.find({}, function (err, users) {
+            if (err) {
+                reject(err);
+            }
+            resolve(users)
+        });
+    });
 }
 
-function allUsersFound(err, users) {
-    if (err) {
-        throw new Error(err);
-    }
-    console.log(users);
+function meteorStartUp() {
+    return new Promise(function(resolve, reject) {
+        Meteor.startup(function () {
+            resolve();
+        });
+    });
 }
+
+/** ACTION FLOWS FROM TOP TO BOTTOM **/
+meteorStartUp()
+    .then(connectToMongooseDB)
+    .then(removeAllUsers)
+    .then(insertTestUsers)
+    .then(findAllUsers)
+    .then(function(users) {
+        console.log(users);
+    })
+    .catch(function(error) {
+        console.error("error from promise catch: ", error);
+    });
 /** END FLOWS FROM TOP TO BOTTOM **/
-
-/* CALLBACK #1 */
-Meteor.startup(meteorStartedUp);
