911a545a38f70691fa1bc561ff411ee123ae5c31
Theuy Limpanont <info@theuy.nl>
2017-02-21 12:22:04 +0100

    refactor(replace anonymous functions with explicit callback names):

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -1,37 +1,52 @@
 import {Meteor} from 'meteor/meteor';
+let mongoose = require('mongoose');
+let userSchema, User, db;
 
-/* CALLBACK #1 */
-Meteor.startup(function () {
-    let mongoose = require('mongoose');
+/** ACTION FLOWS FROM TOP TO BOTTOM **/
+function meteorStartedUp() {
+
     mongoose.connect('mongodb://localhost:3001/local');
-    let userSchema = mongoose.Schema({
+    userSchema = mongoose.Schema({
         name: String
     });
-    let User = mongoose.model('users', userSchema);
-    let db = mongoose.connection;
+    User = mongoose.model('users', userSchema);
+    db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
+
     /* CALLBACK #2 */
-    db.once('open', function () {
-        /* CALLBACK #3 */
-        User.remove({}, function (err, users) {
-            if (err) {
-                throw new Error(err);
-            }
-            let theuy = new User({name: "Theuy Limpanont"});
-            let hans = new User({name: "Hans Kramer"});
-            /* CALLBACK #4 */
-            User.collection.insert([theuy, hans], function (err, docs) {
-                if (err) {
-                    throw new Error(err);
-                }
-                /* CALLBACK #5 */
-                User.find({}, function (err, users) {
-                    if (err) {
-                        throw new Error(err);
-                    }
-                    console.log(users);
-                });
-            });
-        });
-    });
-});
+    db.once('open', mongooseDBOpened);
+}
+
+function mongooseDBOpened() {
+    /* CALLBACK #3 */
+    User.remove({}, allUsersRemoved);
+}
+
+function allUsersRemoved(err, users) {
+    if (err) {
+        throw new Error(err);
+    }
+    let theuy = new User({name: "Theuy Limpanont"});
+    let hans = new User({name: "Hans Kramer"});
+    /* CALLBACK #4 */
+    User.collection.insert([theuy, hans], testUsersInserted);
+}
+
+function testUsersInserted(err, docs) {
+    if (err) {
+        throw new Error(err);
+    }
+    /* CALLBACK #5 */
+    User.find({}, allUsersFound);
+}
+
+function allUsersFound(err, users) {
+    if (err) {
+        throw new Error(err);
+    }
+    console.log(users);
+}
+/** END FLOWS FROM TOP TO BOTTOM **/
+
+/* CALLBACK #1 */
+Meteor.startup(meteorStartedUp);
