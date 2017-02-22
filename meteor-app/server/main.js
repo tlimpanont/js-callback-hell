b1d3586afad6d068e34d70104da7212f3a7a6208
Theuy Limpanont <info@theuy.nl>
2017-02-22 12:45:33 +0100

    docs(server/main.js): set callback notes into the right position

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -3,6 +3,8 @@ let mongoose = require('mongoose');
 let userSchema, User, db;
 
 /** ACTION FLOWS FROM TOP TO BOTTOM **/
+
+/* CALLBACK #1 */
 function meteorStartedUp() {
 
     mongoose.connect('mongodb://localhost:3001/local');
@@ -13,33 +15,33 @@ function meteorStartedUp() {
     db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
 
-    /* CALLBACK #2 */
     db.once('open', mongooseDBOpened);
 }
 
+/* CALLBACK #2 */
 function mongooseDBOpened() {
-    /* CALLBACK #3 */
     User.remove({}, allUsersRemoved);
 }
 
+/* CALLBACK #3 */
 function allUsersRemoved(err, users) {
     if (err) {
         throw new Error(err);
     }
     let theuy = new User({name: "Theuy Limpanont"});
     let hans = new User({name: "Hans Kramer"});
-    /* CALLBACK #4 */
     User.collection.insert([theuy, hans], testUsersInserted);
 }
 
+/* CALLBACK #4 */
 function testUsersInserted(err, docs) {
     if (err) {
         throw new Error(err);
     }
-    /* CALLBACK #5 */
     User.find({}, allUsersFound);
 }
 
+/* CALLBACK #5 */
 function allUsersFound(err, users) {
     if (err) {
         throw new Error(err);
@@ -48,5 +50,4 @@ function allUsersFound(err, users) {
 }
 /** END FLOWS FROM TOP TO BOTTOM **/
 
-/* CALLBACK #1 */
 Meteor.startup(meteorStartedUp);
